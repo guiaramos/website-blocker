6a133d9a4946f9ab6a476db35b6812a9258f1cca
Guilherme <gui_aramos@outlook.com>
2019-07-09 15:26:35 +0900

    Implementing second part

--- website-blocker.py
+++ website-blocker.py
@@ -17,8 +17,8 @@ website_list = "site-list.txt"
 
 # Script variables
 curTime = dt.now()
-startTime = dt(curTime.year, curTime.month, curTime.day, 8) # Start of blocking time
-endTime = dt(curTime.year, curTime.month, curTime.day, 16) # End of blocking time
+startTime = dt(curTime.year, curTime.month, curTime.day, 16) # Start of blocking time
+endTime = dt(curTime.year, curTime.month, curTime.day, 17) # End of blocking time
 
 # Start script #
 
@@ -27,6 +27,8 @@ while True:
     
     # Check the if the current time is between the start and end blocking time
     if startTime < curTime < endTime:
+
+        # Writting the websites on the hosts file
         print ("Working hours...")
         with open(hosts_path, 'r+') as file: # open host file
             content = file.read() # read the content and assign to the variable
@@ -37,7 +39,19 @@ while True:
                         pass
                     else:
                         file.write(redirect + " " + website + "\n") # write each website on the list
+
     else:
+
+        # Removing the websites from the hosts file
+        with open(hosts_path, 'r+') as file:
+            content = file.readlines() # Assigning the lines of the file on variable
+            file.seek(0) # Moving the pointer to the first char
+            with open(website_list, "r") as srcFile: # Opening website list
+                websites = [line.rstrip('\n') for line in srcFile] # Creating a list of the websites written on the file
+                for line in content: # Loop for each line on the hosts file
+                    if not any(website in line for website in websites): # Check if there is not the website on that line
+                        file.write(line) # If there is no website, then write the line
+                file.truncate() # Erase all the remaining content on the file
         print ("Fun hours...")
     
     # check time
