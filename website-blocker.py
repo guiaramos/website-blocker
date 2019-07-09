ff57b2585f28f579bf3c3569fe89e1ab9a58d8c3
Guilherme <gui_aramos@outlook.com>
2019-07-09 13:19:26 +0900

    Setting up the infinite loop

--- website-blocker.py
+++ website-blocker.py
@@ -1,9 +1,29 @@
+# Libraries
 import time
+from datetime import datetime as dt
 
-hosts_path=r"C:\Windows\System32\drivers\etc" # Mac: etc\host
+# Variables #
+# Hosts variables
+hosts_path=r"C:\Windows\System32\drivers\etc"
 redirect = "127.0.0.1"
 website_list = "site-list.txt"
 
+# Script variables
+curTime = dt.now()
+startTime = dt(curTime.year, curTime.month, curTime.day, 8) # Start of blocking time
+endTime = dt(curTime.year, curTime.month, curTime.day, 16) # End of blocking time
+
+# Start script #
+
+# Infinite loop (makes the program run automatically)
 while True:
-    print (1)
+    
+    if startTime < curTime < endTime:
+        print ("Working hours...")
+    else:
+        print ("Fun hours...")
+    
+    # check time
     time.sleep(5)
+
+# End script #
